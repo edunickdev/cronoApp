c905adb32bf842c1522c4bf3390f34872a13abe9
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2024-04-23 07:41:10 -0500

    update

--- cronoapp/lib/presentation/shared/custom_navbar.dart
+++ cronoapp/lib/presentation/shared/custom_navbar.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:cronoapp/domain/entities/person_model.dart';
 import 'package:cronoapp/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,11 +14,28 @@ class CustomNavBarWidget extends ConsumerWidget {
     final diagonal = sqrt(pow(MediaQuery.of(context).size.height, 2) -
         pow(MediaQuery.of(context).size.width, 2));
 
+    void resetValues(WidgetRef ref){
+      final current = PersonConfig(
+            id: 0,
+            title: "00:00",
+            exerciseDurationTime: "00:00",
+            breakDurationTime: "00:00",
+            cycles: 0,
+          );
+          ref.read(currentConfig.notifier).state = current;
+          ref.read(secsBreak.notifier).state = current.breakDurationTime.split(":")[1];
+          ref.read(minsBreak.notifier).state = current.breakDurationTime.split(":")[0];
+          ref.read(secsExercise.notifier).state = current.exerciseDurationTime.split(":")[1];
+          ref.read(minsExercise.notifier).state = current.exerciseDurationTime.split(":")[0];
+          ref.read(cyclesAmount.notifier).state = current.cycles;
+    }
+
     return NavigationBar(
       height: diagonal * 0.13,
       selectedIndex: currentIndex,
       onDestinationSelected: (value) {
         ref.read(selectScreen.notifier).state = value;
+        ref.watch(selectScreen) == 0 ? resetValues(ref) : null;
       },
       destinations: const [
         NavigationDestination(icon: Icon(Icons.home), label: "General"),
